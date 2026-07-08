4946d553659382686294c71da9c7459ad78342fd
fawad-kahn <ifawadkahn@gmail.com>
2026-07-08 18:59:35 +0500

    Update Resume

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const projectsData = [
   },
   {
     id: 4,
-    title: "Brightly",
+    title: "Brightly – Business Landing Page",
     description:
       "This landing page is designed to present a business or service in a clean and professional way. It features a responsive layout with sections for the hero, categories, services, about, contact, and footer, making it easy for visitors to explore information.",
     image: "./brightly.png",
